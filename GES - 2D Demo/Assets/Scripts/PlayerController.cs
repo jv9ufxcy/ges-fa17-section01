c9ebefd3ce0c6115984da95ebab39eefff5641c3
Jaire Vaughn <Jaire.Vaughn@iam.local>
2017-09-25 18:21:19 -0500

    JUMP PHYSICS

---
+++ GES - 2D Demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    [SerializeField] private float speed;
+    [SerializeField] private Animator anim;
+    //private bool isFacingRight;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dirx = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(dirx, 0) * speed * Time.deltaTime;
+        anim.SetFloat("Speed", Mathf.Abs(dirx));
+        if (dirx > 0)
+            this.GetComponent<SpriteRenderer>().flipX =false;
+        else if (dirx < 0)
+            this.GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+
+}
---
+++ GES - 2D Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float jumpStrength = 1f;
+
+    [SerializeField]
+    float movementSpeed = 1f;
+
+    [SerializeField]
+    Transform groundDetectPoint;
+
+    [SerializeField]
+    float groundDetectRadius = 0.2f;
+
+    [SerializeField]
+    LayerMask whatCountsAsGround;
+
+    private bool isOnGround;
+
+    Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateIsOnGround();
+        Move();
+        Jump();
+    }
+
+    private void UpdateIsOnGround()
+    {
+        Collider2D[] groundObjects = Physics2D.OverlapCircleAll(groundDetectPoint.position, groundDetectRadius, whatCountsAsGround);
+
+        isOnGround = groundObjects.Length > 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+    private void Jump()
+    { 
+        if (Input.GetButtonDown("Jump")&& isOnGround)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpStrength);
+            isOnGround = false;
+        }
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
+        Debug.Log("Horizontal input: " + horizontalInput);
+    }
+}
